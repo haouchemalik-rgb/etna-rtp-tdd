440713d74b292ed6b666bdf6353eea6805c5e65a
haouch_m <malik@malik>
2025-01-12 19:02:41 +0100

    delete

--- projet_libre/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('monstermon', 'malik', 'malik', {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false,
-});
-
-module.exports = sequelize;
